0257290e185233c8dcbd9d547700fdb9c30af3ee
Rodro <rodrigo.merino@mulesoft.com>
2018-04-11 16:45:10 -0300

    fix

--- mule-migration-tool-api/src/main/java/com/mulesoft/tools/migration/step/category/ExpressionMigrator.java
+++ mule-migration-tool-api/src/main/java/com/mulesoft/tools/migration/step/category/ExpressionMigrator.java
@@ -7,7 +7,7 @@
 package com.mulesoft.tools.migration.step.category;
 
 /**
- * An interface to handle the step execution
+ * An interface to handle the migration of expressions
  *
  * @author Mulesoft Inc.
  * @since 1.0.0
