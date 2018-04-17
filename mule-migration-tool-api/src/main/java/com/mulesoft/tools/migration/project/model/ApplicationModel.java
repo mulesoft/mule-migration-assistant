b0ffc95f34b7963f1a24828956933127b4dffa08
julianpascualMuleSoft <julian.pascual@mulesoft.com>
2018-04-17 11:03:33 -0300

    MMT-94 Changes due to CR

--- mule-migration-tool-api/src/main/java/com/mulesoft/tools/migration/project/model/ApplicationModel.java
+++ mule-migration-tool-api/src/main/java/com/mulesoft/tools/migration/project/model/ApplicationModel.java
@@ -8,7 +8,11 @@ package com.mulesoft.tools.migration.project.model;
 
 import com.mulesoft.tools.migration.project.model.artifact.MuleArtifactJsonModel;
 import com.mulesoft.tools.migration.project.model.pom.PomModel;
-import org.jdom2.*;
+import org.jdom2.Attribute;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
 import org.jdom2.filter.Filters;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.xpath.XPathExpression;
