b35d336453bf4ec6f3907e890ef6a12ec844e7ed
Luciano RaineriMarchina <luciano.raineri@mulesoft.com>
2019-01-02 17:43:46 -0300

    checking enableCorrelation attribute

--- mule-migration-tool-library/src/main/java/com/mulesoft/tools/migration/library/mule/steps/splitter/AbstractSplitter.java
+++ mule-migration-tool-library/src/main/java/com/mulesoft/tools/migration/library/mule/steps/splitter/AbstractSplitter.java
@@ -5,8 +5,6 @@ import static com.mulesoft.tools.migration.library.mule.steps.vm.AbstractVmEndpo
 import static com.mulesoft.tools.migration.step.util.XmlDslUtils.CORE_NAMESPACE;
 import static com.mulesoft.tools.migration.step.util.XmlDslUtils.addElementAfter;
 import static com.mulesoft.tools.migration.step.util.XmlDslUtils.addElementBefore;
-import static java.lang.Math.abs;
-import static java.nio.file.Paths.get;
 import static java.util.Optional.empty;
 import static org.jdom2.Namespace.getNamespace;
 
@@ -41,28 +39,56 @@ public abstract class AbstractSplitter extends AbstractApplicationModelMigration
   @Override
   public void execute(Element splitter, MigrationReport report) throws RuntimeException {
     SplitterAggregatorInfo splitterAggregatorInfo = new SplitterAggregatorInfo(splitter, getApplicationModel());
-    List<Element> objectAndSiblings = splitter.getParentElement().getChildren();
+    List<Element> splitterAndSiblings = splitter.getParentElement().getChildren();
     List<Element> elementsBetweenSplitterAndAggregator = new ArrayList<>();
+    Element aggregator = null;
     boolean shouldAdd = false;
-    for(Element element : objectAndSiblings) {
+    handleNeverEnableCorrelation(splitter, report);
+    for(Element element : splitterAndSiblings) {
       if(element.equals(splitter)) {
         shouldAdd = true;
         continue;
       }
-      if(getMatchingAggregatorName().equals(element.getName())) {
-        element.detach();
+      if(foundMatchingAggregator(element)) {
+        aggregator = element;
         break;
       }
       if(shouldAdd) {
-        element.detach();
         elementsBetweenSplitterAndAggregator.add(element);
       }
     }
-    addElementBefore(getSetPayloadSizeVariableElement(splitterAggregatorInfo), splitter);
-    addElementBefore(wrapWithForEachAndAggregator(splitterAggregatorInfo, elementsBetweenSplitterAndAggregator), splitter);
-    addElementAfter(getVmConsumeElement(splitterAggregatorInfo), splitter);
-    addVmQueue(splitterAggregatorInfo);
-    splitter.detach();
+    if(aggregator == null) {
+      //Splitter has no aggregator.
+    }else if(isCustomAggregator(aggregator)) {
+      //Splitter has a custom aggregator
+      handleCustomAggregator(splitter, aggregator, report);
+    }else {
+      //Splitter has a matching aggregator
+      addElementBefore(getSetPayloadSizeVariableElement(splitterAggregatorInfo), splitter);
+      addElementBefore(wrapWithForEachAndAggregator(splitterAggregatorInfo, elementsBetweenSplitterAndAggregator), splitter);
+      addElementAfter(getVmConsumeElement(splitterAggregatorInfo), splitter);
+      addVmQueue(splitterAggregatorInfo);
+      for(Element element : elementsBetweenSplitterAndAggregator) {
+        element.detach();
+      }
+      aggregator.detach();
+      splitter.detach();
+    }
+  }
+
+  private boolean foundMatchingAggregator(Element element) {
+    return isCustomAggregator(element) || getMatchingAggregatorName().equals(element.getName());
+  }
+
+  private void handleCustomAggregator(Element splitter,Element aggregator, MigrationReport report) {
+    report.report("splitter.aggregator.custom", splitter, aggregator);
+  }
+
+  private void handleNeverEnableCorrelation(Element splitter, MigrationReport report) {
+    String enableCorrelation = splitter.getAttributeValue("enableCorrelation");
+    if("NEVER".equals(enableCorrelation)) {
+      report.report("splitter.correlation.never", splitter, splitter);
+    }
   }
 
   private Element wrapWithForEachAndAggregator(SplitterAggregatorInfo splitterAggregatorInfo, List<Element> elements) {
@@ -72,14 +98,15 @@ public abstract class AbstractSplitter extends AbstractApplicationModelMigration
     return forEachElement;
   }
 
-  private boolean isCustomAggregator(String elementName) {
-    return "custom-aggregator".equals(elementName);
+  private boolean isCustomAggregator(Element element) {
+    return "custom-aggregator".equals(element.getName());
   }
 
   private Element getAggregatorElement(SplitterAggregatorInfo splitterAggregatorInfo) {
     return AGGREGATOR_TEMPLATE
             .clone()
             .setAttribute("name", splitterAggregatorInfo.getAggregatorName())
+            .setAttribute("groupSize", "#[vars." + splitterAggregatorInfo.getGroupSizeVariableName() + "]")
             .addContent(
                     new Element("aggregation-complete", AGGREGATORS_NAMESPACE)
                             .addContent(new Element("publish", VM_NAMESPACE)
@@ -91,7 +118,7 @@ public abstract class AbstractSplitter extends AbstractApplicationModelMigration
   private Element getSetPayloadSizeVariableElement(SplitterAggregatorInfo splitterAggregatorInfo) {
     return SET_VARIABLE_TEMPLATE
             .clone()
-            .setAttribute("variableName", splitterAggregatorInfo.getGrupSizeVariableName());
+            .setAttribute("variableName", splitterAggregatorInfo.getGroupSizeVariableName());
   }
 
   private Element getVmConsumeElement(SplitterAggregatorInfo splitterAggregatorInfo) {
@@ -104,7 +131,7 @@ public abstract class AbstractSplitter extends AbstractApplicationModelMigration
   private void addVmQueue(SplitterAggregatorInfo splitterAggregatorInfo) {
     Element vmConfig = migrateVmConfig(splitterAggregatorInfo.getSplitterElement(), empty(), splitterAggregatorInfo.getVmConfigName(), getApplicationModel());
     Element queues = vmConfig.getChild("queues", VM_NAMESPACE);
-    queues.addContent(VM_QUEUE_TEMPLATE.clone().setAttribute("name", splitterAggregatorInfo.getVmQueueName()));
+    queues.addContent(VM_QUEUE_TEMPLATE.clone().setAttribute("queueName", splitterAggregatorInfo.getVmQueueName()));
   }
 
 }
