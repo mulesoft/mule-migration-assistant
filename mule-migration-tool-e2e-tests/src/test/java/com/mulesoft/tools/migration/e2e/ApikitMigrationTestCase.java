39b45228ebea9580107935976df0a96cb197d8f9
Jesica Fera <jfera@mulesoft.com>
2022-03-23 13:07:36 -0700

    Add license header

--- mule-migration-tool-e2e-tests/src/test/java/com/mulesoft/tools/migration/e2e/ApikitMigrationTestCase.java
+++ mule-migration-tool-e2e-tests/src/test/java/com/mulesoft/tools/migration/e2e/ApikitMigrationTestCase.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2020, Mulesoft, LLC. All rights reserved.
+ * Use of this source code is governed by a BSD 3-Clause License
+ * license that can be found in the LICENSE.txt file.
+ */
 package com.mulesoft.tools.migration.e2e;
 
 import org.junit.Test;
